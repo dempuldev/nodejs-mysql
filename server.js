1f9b550510cafb5fc2cdb5040e496902adb68b2d
dempuldev <1903040037@ump.ac.id>
2022-06-09 09:02:36 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+// Load library dari package.json pada dependency
+const express = require("express");
+const mysql = require("mysql");
+const BodyParser  = require("body-parser");
+
+// Set port and url
+const app = express();
+const port = process.env.PORT || 8000 // Untuk deploy online
+
+app.use(BodyParser.urlencoded({ extended: true
+}));
+
+// Buat koneksi ke database
+const db = mysql.createConnection({
+    host: "localhost",
+    database: "quiz",
+    user: "root",
+    password: "",
+});
+
+// Set template pada directory views
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// Pengaturan laman api ada disini
+db.connect((err) => {
+    if (err) throw err // Pesan error
+    console.log("Database connected!") // Pesan sukses konek
+
+    app.get("/", (req, res) => {
+        // const sql = "SELECT * FROM m_penduduk"
+        const sql_dinamis = "SELECT a.NIK, a.Nama, b.Nama_Kecamatan, c.Nama_Kabupaten, d.Nama_Provinsi FROM m_penduduk a INNER JOIN l_kecamatan b ON a.Kd_Kecamatan = b.Kd_Kecamatan INNER JOIN l_kabupaten c ON a.Kd_Kabupaten = c.Kd_Kabupaten INNER JOIN l_provinsi d ON a.Kd_Provinsi = d.Kd_Provinsi"
+        db.query(sql_dinamis, (err, result) => { 
+            const m_penduduk = JSON.parse(JSON.stringify(result)) 
+            res.render("index", { m_penduduk: m_penduduk, title: "CRUD NodeJS Mysql", table: "Tampil Data Database" })
+        })
+        // console.log("Hasil yang keluar -> ", penduduk) // Test di command
+    });
+
+});
+
+// Test buka di browser
+// app.get("/", (req, res) => {
+//     res.send("Halaman berhasil di buka")
+// });
+
+// Setting server
+app.listen(port, () => console.log(
+    `Listening on port ${port}`)
+    )
